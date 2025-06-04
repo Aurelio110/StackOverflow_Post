3128760720046a3b517391dbfc7f6315a4307eb5
aurelio.eissner <aurelio.eissner@DHM-1168>
2025-06-04 13:25:20 +0200

    Projektdateien hinzufügen.

---
+++ StackOverflow_Post/Post.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow_Post
+{
+    class Post
+    {
+        private string title;
+        private string describtion;
+        private DateTime dateCreated;
+        private int votes = 0;
+
+        public Post(string title, string description)
+        {
+            this.title = title;
+            this.describtion = description;
+            this.dateCreated = DateTime.Now;
+        }
+        public Post(string title, string description, int votes)
+            : this(title, description)
+        {
+            this.votes = votes;
+        }
+            
+
+       
+
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Describtion
+        {
+            get { return describtion; }
+            set { describtion = value; }
+        }
+        public DateTime DateCreated
+        {
+            get { return dateCreated; }
+            set { dateCreated = value; }
+        }
+
+        public int Votes
+        {
+            get { return votes; }
+            set { votes = value; }
+        }
+
+        public int upvote()
+        {
+            votes++;
+            return votes;
+        }
+        public int downvote()
+        {
+            if (votes > 0)
+            {
+                votes--;
+            }
+            return votes;
+        }
+
+
+    }
+}
---
+++ StackOverflow_Post/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+
+
+namespace StackOverflow_Post
+{
+    class main
+    {
+        public static void Main(string[] args)
+        {
+            var post1 = new Post("My First Post", "This is the description of my first post.", 1);
+            var post2 = new Post("My Second Post", "This is the description of my second post.", 2);
+
+            Console.WriteLine(post1.DateCreated);
+            Console.WriteLine(post1.Votes);
+
+            post1.Votes = 5;
+            post1.Title = "Updated Post Title";
+            post1.Describtion = "Updated description for my first post.";
+            post1.upvote();
+            post1.downvote();
+            Console.WriteLine(post1.Votes);
+        }
+    }
+}
